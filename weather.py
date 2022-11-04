71d279ba14de36458a7a016dd2946b9d4cd801e3
Black09OQ <0.bl.a.c.k.0.9oq@gmail.com>
2022-11-04 17:42:12 +0900

    おｋ

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
+import grovepi
 import datetime
 import requests
 import json
@@ -9,28 +10,28 @@ res = requests.get(url)
 res = res.text
 res = json.loads(res)
 
+grovepi.set_bus("RPI_1")
+
 
 # 現在時刻を取得
 dt_now = datetime.datetime.now()
 
-# 取得した天気jsonファイルを開く 
-weather_open = open("text.json","r")
-# jsonとして読み込み
-weather_load = json.load(weather_open)
-print(type(weather_load))
+
 
 # WMOcodeJsonを読み込み
 WMO_open = open("WMO.json","r")
 WMO_load = json.load(WMO_open)
 
 
-weathercode = weather_load['hourly']['weathercode'][dt_now.hour + 1]
+weathercode = res['hourly']['weathercode'][dt_now.hour + 1]
 
 weather = WMO_load[str(weathercode)]
 
 temp = ""
 
 for i in weather:
-    temp += dict.dict{i}
+    temp += dict.dict[i]
 
 print(temp)
+
+setText(temp)
